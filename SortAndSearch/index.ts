19e46921fe3c9900687f0ab94ad97e61225f9f89
Piotr Frankowski <piotr.f@lorentus.com>
2019-08-09 12:59:26 +0200

    add second task

---
+++ SortAndSearch/index.ts
@@ -0,0 +1,5 @@
+import { ASort } from './ASort';
+import { BSort } from './BSort';
+
+const unsorted = [13, 2, 17, 5, 77, 22, 83, 65, 14, 9, 0, 4, 7, 32];
+const elementsToFind = [1, 5, 13, 27, 77];
